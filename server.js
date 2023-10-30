9bbab3f178ff5b2f8b465281dd011a86572d9342
Marina Lebedeva <marina.j.lebedeva@gmail.com>
2023-10-30 03:52:09 +0100

    fixed tests issues

--- server.js
+++ server.js
@@ -13,3 +13,4 @@ app.use('/*',(req,res)=>{res.sendFile(path.join(__dirname,'dist/index.html'))});
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
   }); 
+  
